2afcae7ae041f6a1c08e4ece007a3de919b690b2
Joe Scott <jbs198585@gmail.com>
2022-06-18 08:45:51 -0400

    Creating reviews

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const { campgroundSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-const ExpressError = require('./utils/ExpressError');
-const { stat } = require('fs');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -77,6 +77,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds');
+}));
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 app.all('*', (req, res, next) => {
