2d0fb72719057acc0be26bda73334288904e470c
Joe Scott <jbs198585@gmail.com>
2022-07-17 14:31:12 -0400

    ReturnTo Behavior

--- routes/users.js
+++ routes/users.js
@@ -30,12 +30,15 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
   req.flash('success', 'Welcome back!');
-  res.redirect('/campgrounds');
+  const redirectUrl = req.session.returnTo || '/campgrounds';
+  res.redirect(redirectUrl);
 });
 
 router.get('/logout', (req, res, next) => {
   req.logout(function(err) {
-    if (err) { return next(err); }
+    if (err) { 
+      return next(err); 
+    }
     req.flash('success', "Logged out.");
     res.redirect('/campgrounds');
   });
