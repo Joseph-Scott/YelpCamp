a7b2c264d39a158eefb90ab6bed96763a01509b5
Joe Scott <jbs198585@gmail.com>
2022-08-12 13:19:38 -0400

    Changed map centering on campgrounds page to Georgia

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'cluster-map',
   style: 'mapbox://styles/mapbox/light-v10',
-  center: [-103.5917, 40.6699],
+  center: [-83.441162, 33.247875],
   zoom: 3
 });
 
